82f124168c3ec68622ac01128f07dbb4ae81e022
Ebenezer Ampiah <mr.ampiah@gmail.com>
2015-10-31 17:03:05 -0500

    no message

---
+++ q/index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>User Sign Up</title>
+    </head>
+    <body>
+        <h2>Please enter sign up details:</h2>
+        <form action="" method="post">
+        </form>
+    </body>
+</html>
