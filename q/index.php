a580c37c799ec3f6ea3245d77f5a55399821fcd4
Ebenezer Ampiah <mr.ampiah@gmail.com>
2015-10-31 17:11:37 -0500

    no message

--- q/index.php
+++ q/index.php
@@ -5,6 +5,13 @@
     <body>
         <h2>Please enter sign up details:</h2>
         <form action="" method="post">
+            <label for="name">Name:</label>
+            <input type="text" width="50px">
+            <label for="address">Address Line 1:</label>
+            <input type="text" width="100px"><br>
+            <label for="city">City:</label>
+            <input type="text" width="50px"><br>
+            <input type="button" value="Join Now!">
         </form>
     </body>
 </html>
