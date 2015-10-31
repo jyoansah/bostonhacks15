32c2b0d17f07d35c206be07425679657e153031e
Ebenezer Ampiah <mr.ampiah@gmail.com>
2015-10-31 18:41:52 -0500

    no message

--- q/index.php
+++ q/index.php
@@ -5,14 +5,14 @@
     </head>
     <body>
         <h2>Please enter sign up details:</h2>
-        <form action="metricsPage.php" method="post">
+        <form action="/metricsPage.php" method="post">
             <label for="name">Name:</label>
             <input type="text" name="name"><br>
             <label for="address">Address Line 1:</label>
             <input type="text" name="address"><br>
             <label for="city">City:</label>
             <input type="text" width="50px" name="city"><br>
-            <input type="button" name="submit" value="Sign Up!">
+            <input type="submit" name="submit" value="Sign Up!">
         </form>
     </body>
 </html>
