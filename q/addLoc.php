e4e8105e4175f7438ca04aa725b91935711daddd
Ebenezer Ampiah <mr.ampiah@gmail.com>
2015-10-31 18:28:23 -0500

    no message

--- q/addLoc.php
+++ q/addLoc.php
@@ -1,5 +1,12 @@
-<?php
+<html>
+    <head>
+        <title>Confirmation Page</title>
+    </head>
+    <body>
+        <?php
 if(isset($_POST['submit'])){
     echo "something nigga";
 }
 ?>
+    </body>
+</html>
