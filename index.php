0fd91f1f8af32b2dbfd9008e12ba7a828287e07e
Jon Tan <jaeteemail@gmail.com>
2015-10-31 20:33:35 -0400

    Added styling for index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <head>
         <meta charset="UTF-8">
         <title>I hate queues!</title>
+        <link rel="stylesheet" type="text/css" href="style/index.css">
     </head>
     <body>
         Do you hate queues? We do too!
