420ac046df63cfaaca23bb0eacf82ca5a3ac8201
Jon Tan <jaeteemail@gmail.com>
2015-10-31 20:46:15 -0400

    Added bootstrap.js

--- index.php
+++ index.php
@@ -9,11 +9,12 @@
         <meta charset="UTF-8">
         <title>I hate queues!</title>
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc= sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
     </head>
     <body>
         <div id="container">
-            <h1>deeque</h1>
-            Do you hate queues? We do too!
+            <p class="text-center"><h1>deeque</h1></p>
+            <p class="text-center"><h3>Do you hate waiting in line? We do too!</h3></p>
             <?php
               $api->api(DB_METHODS);
             ?>
