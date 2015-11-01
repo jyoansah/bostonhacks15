938dfcc0eddcc134bf88e3e8dcc5c671eeb51a72
Jeffery Yaw Owusu- Ansah <jyoansah@gmail.com>
2015-11-01 11:27:00 -0500

    Test

--- sms.php
+++ sms.php
@@ -3,7 +3,7 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
     header("access-control-allow-origin: *");
     include_once 'api/api.php';
-    global $conn, $api;
+    global $conn, $body $api;
     session_start();
 
     if(isset($_REQUEST['Body'])){
@@ -16,7 +16,7 @@
     <Message>Hello, Welcome to Deeque
     Please select a Queue:
         <?php
-        echo $body;
+            echo $body;
             $queues = getQueues($conn);
             foreach ($queues as $queue) {
                 echo $queue->id." --> ".$queue->name."\n";
