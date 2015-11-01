42e763f63127fe511243f54c169559c457b0938b
Ebenezer Ampiah <mr.ampiah@gmail.com>
2015-10-31 19:28:25 -0500

    no message

--- q/metricsPage.php
+++ q/metricsPage.php
@@ -14,6 +14,8 @@
             </span>
         </header>
         <p>Current Customer: #6</p>
+        <p>Number in line: 20</p>
+        <p>Average serve time: 3:00:22</p>
         <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
             <input type="submit" value="Next" name="submit">
         </form>
