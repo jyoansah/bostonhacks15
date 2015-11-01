b85d5f1c7e7abd69b5acef8f31af6544eac20569
Jeffery Yaw Owusu- Ansah <jyoansah@gmail.com>
2015-11-01 10:57:08 -0500

    SMSSS

--- sms.php
+++ sms.php
@@ -1,21 +1,21 @@
 <?php
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
     header("access-control-allow-origin: *");
     include_once 'api/api.php';
     global $conn, $api;
     session_start();
-    header("content-type: text/xml");
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 
 
 
 <Response>
-    <Message>Hello, Welcome to Deeque </br>
+    <Message>Hello, Welcome to Deeque
     Please select a Queue:<br>
         <?php
             $queues = getQueues($conn);
             foreach ($queues as $queue) {
-                echo $queue->id.' --> '.$queue->name.'</br>';
+                echo $queue->id.' --> '.$queue->name;
             }
         ?>
     </Message>
