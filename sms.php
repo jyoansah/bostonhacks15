5f5220e16fd3e7f3447f79fe217fba177cfe8640
Jeffery Yaw Owusu- Ansah <jyoansah@gmail.com>
2015-11-01 10:10:29 -0500

    SMS

--- sms.php
+++ sms.php
@@ -4,6 +4,22 @@ header("access-control-allow-origin: *");
 include_once 'api/api.php';
 global $conn, $api;
 ?>
+
+<?php
+function listQs()
+{
+    $queues = getqueues($this->conn);
+    foreach ($queues as $queue) {
+        echo 'Queue ID: ' . $queue->id . ' --> ' . $queue->name . '<br>';
+        //echo '<a href="restaurant.php/?id='.$queue["id"].'">'."sup".$queue['name'].'</a>"';
+    }
+}
+?>
+
 <Response>
-    <Message>Hello, Mobile Monkey</Message>
+    <Message>Hello, Welcome to Deeque<br>
+    Please select a queue:
+
+
+    </Message>
 </Response>
