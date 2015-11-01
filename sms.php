7860206a35a5585e4abff289114e374222146a12
Jeffery Yaw Owusu- Ansah <jyoansah@gmail.com>
2015-11-01 10:37:36 -0500

    sms

--- sms.php
+++ sms.php
@@ -1,26 +1,20 @@
 <?php
-        echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-        header("access-control-allow-origin: *");
-        include_once 'api/api.php';
-        global $conn, $api;
-
-        function listQs()
-        {
-            $ret = "";
-            $queues = getqueues($this->conn);
-            foreach ($queues as $queue) {
-                $ret =  $ret.'Queue ID: ' . $queue->id . ' --> ' . $queue->name . '<br>';
-            }
-
-            return $ret;
-        }
+header("access-control-allow-origin: *");
+include_once 'api/api.php';
+global $conn, $api;
+session_start();
 ?>
+
+
 
 <Response>
     <Sms>Hello, Welcome to Deeque<br>
     Please select a queue:
         <?php
-            echo listQs();
+            $queues = getqueues($this->conn);
+            foreach ($queues as $queue) {
+                echo 'Queue ID: '.$queue->id.' --> '.$queue->name.'<br>';
+            }
         ?>
     </Sms>
 </Response>
