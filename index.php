e973a106124cfad1721cf7577d88da0c03e1d209
Jon Tan <jaeteemail@gmail.com>
2015-10-31 20:49:46 -0400

    Minor styling changes

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
     <head>
         <meta charset="UTF-8">
         <title>I hate queues!</title>
-        
+
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <div id="container">
+        <div class="container">
             <p class="text-center"><h1>deeque</h1></p>
             <p class="text-center"><h3>Do you hate waiting in line? We do too!</h3></p>
             <?php
