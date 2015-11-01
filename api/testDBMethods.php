f80ed85cb5aadb19443749cd38029abd462a1f1e
DaCardinal <daniel.quaidoo@gmail.com>
2015-11-01 20:04:08 +0000

    testing select method

--- api/testDBMethods.php
+++ api/testDBMethods.php
@@ -5,8 +5,14 @@ function ReadData($conn) {
     {
         $tsql = "SELECT [Name] FROM deeque.Queue";
         $getProducts = sqlsrv_query($conn, $tsql);
-        if ($getProducts == FALSE)
+        
+        if ($getProducts == FALSE) {
+            echo "read f";
             die(FormatErrors(sqlsrv_errors()));
+        }else{
+            echo "read success";
+        }
+        
         $productCount = 0;
         while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))
         {
