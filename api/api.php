3a16bd90d69c886d45fc5570b8984dd0b8733b40
DaCardinal <daniel.quaidoo@gmail.com>
2015-11-01 18:21:44 +0000

    first commit
    first commit

---
+++ api/api.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once 'controller.php';
+
+class api {
+
+    public $conn;
+
+    // Constructor - open DB connection
+    function __construct() {
+        global $conn;
+        $this->conn = $conn;
+    }
+
+    // Destructor - close DB connection
+    function __destruct() {
+        $this->conn = null;
+    }
+    
+    // API interface
+    function api($action = null, $msg = null) {
+
+        if ((isset($action) && isset($msg)) || isset($action)) {
+            
+            //NQAssertSession();
+            switch ($action) {
+                case API_CALL:
+                        echo "This is a test api call";
+                    break;
+
+                //Aux command calls
+                case SET_FEEDBACK_MSG:
+                    setFeedbackMessage($msg);
+                    break;
+
+                default : sanitizeResult('Invalid Action', 400);
+                    break;
+            }
+        } else {
+            sanitizeResult('Invalid Request', 400);
+        }
+    }
+
+}
+
+$api = new api();
