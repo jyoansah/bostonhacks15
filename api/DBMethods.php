76f1f0da0fd085d390492c5173d5c3c8dea8cd42
Jon Tan <jaeteemail@gmail.com>
2015-11-01 05:44:23 -0500

    test

--- api/DBMethods.php
+++ api/DBMethods.php
@@ -218,6 +218,7 @@ function deQueueUser($conn, $queue_id){
                  JOIN(SELECT MIN(position) AS min_queue_pos FROM dbo.Users) user2
                  WHERE user2.min_queue_pos = dbo.Users.position 
                  AND dbo.Users.queue_id = ".$queue_id;
+        echo $tsql;
         $results = sqlsrv_query($conn, $tsql);
 
         if ($results == FALSE) {
