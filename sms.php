e72764643c903ba060c6e127a2d60d97ce3bf3dc
Jeffery Yaw Owusu- Ansah <jyoansah@gmail.com>
2015-11-01 10:40:46 -0500

    sms

--- sms.php
+++ sms.php
@@ -9,10 +9,13 @@ session_start();
 
 <Response>
     <Sms>Hello, Welcome to Deeque<br>
-    Please select a 2queue:
+    Please select a 3queue:
         <?php
+            echo 'here';
             $queues = getqueues($this->conn);
+            echo 'here1'
             foreach ($queues as $queue) {
+                echo 'here2'
                 echo 'Queue ID: '.$queue->getId().' --> '.$queue->getName().'<br>';
             }
         ?>
