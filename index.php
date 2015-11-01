58fe1919cb8ec732eb589b035b7e8ca25e35fe78
Jon Tan <jaeteemail@gmail.com>
2015-10-31 20:40:57 -0400

    Changed to bootstrap

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
     <head>
         <meta charset="UTF-8">
         <title>I hate queues!</title>
-        <link rel="stylesheet" type="text/css" href="style/index.css">
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     </head>
     <body>
-        <div id="main_container">
-            <div id="title">deeque</div>
+        <div id="container">
+            <h1>deeque</h1>
             Do you hate queues? We do too!
             <?php
               $api->api(DB_METHODS);
