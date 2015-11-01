dad965a6929a8d0ed55c3112a50b9d09eef9e271
Jon Tan <jaeteemail@gmail.com>
2015-11-01 06:45:45 -0500

    test

--- customer.php
+++ customer.php
@@ -8,22 +8,16 @@
 
 <?php
 	//Get current customer
-	$query = mysql_fetch_row(mysql_query("SELECT queue_position FROM user" WHERE ));
 	if(isset($_POST['new_customer'])){
 		try{
-			$result = mysql_fetch_row(mysql_query("INSERT INTO user (queue_id) VALUES (".$_SESSION['queue_id'].")"));		
+			$new_user = newUser();
+			$results = addUser($new_user);
+			echo "Your queue number is:". $results;
 		}
 		catch(Exception $e){
 			echo $e->getMessage();
 		}
 	}
-	try{
-		$result = mysql_fetch_row(mysql_query("SELECT * FROM user ORDER BY queue_position DESC LIMIT 1 WHERE queue_id=".$_SESSION['queue_id']));
-		$current_user = $result['queue_position'];	
-	}
-		catch(Exception $e){
-			echo $e->getMessage();
-		}
 ?>
 <html>
     <head>
@@ -31,7 +25,6 @@
         <title>Customer</title>
     </head>
     <body>
-    	Now serving: <?php echo $current_user; ?>
     	<form method="POST" action="">
         	<button name="new_customer" value="submit">Get Number</button>
         </form>
