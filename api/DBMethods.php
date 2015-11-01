d150289fe544db23593bdadd28b37963a07975e5
Jeffery Yaw Owusu- Ansah <jyoansah@gmail.com>
2015-11-01 08:38:20 -0500

    Fixed db method connections

--- api/DBMethods.php
+++ api/DBMethods.php
@@ -46,6 +46,7 @@
                 $tsql = "SELECT [id],[Name],[Location] FROM dbo.Queue WHERE $condition";
 
             }
+            $conn = OpenConnection();
             $getQueues = sqlsrv_query($conn, $tsql);
             if ($getQueues == FALSE) {
                 echo("Error!!<br>");
@@ -61,6 +62,7 @@
             }
 
             sqlsrv_free_stmt($getQueues);
+            sqlsrv_close($conn);
 
             if (!empty($queues)) {
                 return  $queues;
@@ -97,6 +99,7 @@
             $tsql = "INSERT INTO dbo.Queue (Name, Location)
                 OUTPUT INSERTED.id VALUES ('$Name','$Location')";
             //Insert query
+            $conn = OpenConnection();
             $insertReview = sqlsrv_query($conn, $tsql);
 
             if ($insertReview == FALSE) {
@@ -139,7 +142,6 @@
                 $tsql = "SELECT [id],[queue_id],[position] FROM dbo.Users WHERE ".$condition;
 
             }
-            echo "<br>TSQL= ".$tsql."<br>";
             $conn = OpenConnection();
             $getUsers = sqlsrv_query($conn, $tsql);
 
@@ -159,6 +161,7 @@
             }
 
             sqlsrv_free_stmt($getUsers);
+            sqlsrv_close($conn);
 
             if (!empty($users)) {
                 return  $users;
@@ -197,6 +200,7 @@
             $tsql = "INSERT INTO dbo.Users (queue_id, position)
                     OUTPUT INSERTED.id VALUES ('$QueueId','$Position')";
             //Insert query
+            $conn = OpenConnection();
             $insertReview = sqlsrv_query($conn, $tsql);
 
             if ($insertReview == FALSE) {
@@ -210,6 +214,7 @@
             }
             sqlsrv_free_stmt($insertReview);
             sqlsrv_close($conn);
+
         }
         catch(Exception $e)
         {
@@ -228,6 +233,8 @@
     function getfirstInLine($conn, $queue_id){
         try{
             $tsql = "SELECT TOP 1 position FROM dbo.Users WHERE queue_id=".$queue_id." ORDER BY position ASC";
+
+            $conn = OpenConnection();
             $results = sqlsrv_query($conn, $tsql);
 
             if ($results == FALSE) {
@@ -236,6 +243,11 @@
 
             $results = sqlsrv_fetch_array($results, SQLSRV_FETCH_ASSOC);
             $result = $results['position'];
+
+            sqlsrv_free_stmt($results);
+            sqlsrv_close($conn);
+
+
             return $result;
         }
         catch(Exception $e){
@@ -246,6 +258,8 @@
     function getLastInLine($conn, $queue_id){
         try{
             $tsql = "SELECT TOP 1 position FROM dbo.Users WHERE queue_id=".$queue_id." ORDER BY position DESC";
+
+            $conn = OpenConnection();
             $results = sqlsrv_query($conn, $tsql);
 
             if ($results == FALSE) {
@@ -254,6 +268,10 @@
 
             $results = sqlsrv_fetch_array($results, SQLSRV_FETCH_ASSOC);
             $result = $results['position'];
+
+            sqlsrv_free_stmt($results);
+            sqlsrv_close($conn);
+
             return $result;
         }
         catch(Exception $e){
@@ -266,11 +284,16 @@
             $tsql = "WITH q AS
             (SELECT TOP 1 *
             FROM dbo.Users ORDER BY position ASC) DELETE FROM q";
+
+            $conn = OpenConnection();
             $results = sqlsrv_query($conn, $tsql);
 
             if ($results == FALSE) {
                 die(print_r( sqlsrv_errors(), true));
             }
+
+            sqlsrv_close($conn);
+
             return $results;
         }
         catch(Exception $e){
