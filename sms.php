c467d40cf9a90e59f288920c702ea7d7162b7211
Jeffery Yaw Owusu- Ansah <jyoansah@gmail.com>
2015-11-01 12:46:53 -0500

    Done

--- sms.php
+++ sms.php
@@ -1,7 +1,38 @@
 <?php
-header("content-type: text/xml");
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+
+    header("access-control-allow-origin: *");
+    include_once 'api/api.php';
+    global $conn, $body, $position, $api;
+    session_start();
+
+    if (isset($_REQUEST['Body'])) {
+        $body = $_REQUEST['Body'];
+        $token = strtok($body, " ");
+
+        if (strtolower($token) == 'join') {
+            $token = strtok(" ");
+            $user = new User(intval($token));
+            $user->setTel($_REQUEST['From']);
+            $position = addUser($conn, $user);
+        }
+    }
 ?>
+
 <Response>
-    <Message>Hello, Mobile Monkey</Message>
+    <Message>Hello, Welcome to Deeque
+        <?php
+        if (empty($position)) {
+            echo "Please select a Queue: \n";
+            $queues = getQueues($conn);
+            foreach ($queues as $queue) {
+                echo $queue->id . " --> " . $queue->name . "\n";
+            }
+        } else {
+            echo "You are num: " . $position;
+
+        }
+        ?>
+    </Message>
 </Response>
