302db207023f8dbb411d80da5994973883909231
Jeffery Yaw Owusu- Ansah <jyoansah@gmail.com>
2015-11-09 20:35:07 -0500

    Css linking

--- index.php
+++ index.php
@@ -24,10 +24,10 @@ global $conn, $api;
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
         <!-- Custom styles for this template -->
-        <link href="style/cover.css" rel="stylesheet">
+        <link href="/style/cover.css" rel="stylesheet">
 
         <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-        <script src="js/ie10-viewport-bug-workaround.js"></script>
+        <script src="/js/ie10-viewport-bug-workaround.js"></script>
 
     </head>
 
